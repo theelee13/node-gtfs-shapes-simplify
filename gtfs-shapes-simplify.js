c2fa6980f4b0f0b98c3b5db4e87d2395fca21088
Edward Lee <theelee13@gmail.com>
2014-06-20 17:04:19 -0400

    re-work for dynamic application

--- gtfs-shapes-simplify.js
+++ gtfs-shapes-simplify.js
@@ -1,14 +1,13 @@
-var fs = require('fs');
-var parse = require('csv-parse');
+/*
+ *	Undergoing a rework. Instead of working with the shapes file directly (and needing the csv-parser and fs)
+ *	we'll now assume an individual shape is passed through with its points in order. 
+ */
+
 var simple = require('simplify-polyline');
 
 module.exports = {
 	simplify: function(loc, epsilon, callback){
-		var polyline = getArray(loc, callback);
-		//insert a method for breaking apart the array into multiple by shape_id. Then wrap this in a loop.
-		var sortedLine = polyLine.sort(compare(a,b));
 		var finalLine = simple.simplify(sortedLine,epsilon);
-		writeToFile(finalLine);
 	}
 }
 
@@ -29,35 +28,4 @@ getArray = function(loc, callback){
 	});
 	filestream.pipe(parser);
 	return coordArray;
-}
-
-getObject = function(record){
-	obj = {};
-	obj.y= +record.shape_pt_lat;
-	obj.x= +record.shape_pt_lon;
-	obj.place = record.shape_pt_sequence;
-	obj.shapeID = record.shape_id;
-	return obj;
-}
-
-writeToFile = function (coords){
-	
-}
-
-compare = function (a,b){
-	if(+a.place < +b.place){
-		return -1;
-	}
-	if(+a.place > +b.place){
-		return 1;
-	}
-	return 0;
-}
-
-getDataArray = function (record){
-	array = [];
-	array.push(record.shape_id);
-	array.push(record.shape_pt_lat);
-	array.push(record.shape_pt_lon);
-	array.push(record.shape_pt_sequence);
 }
